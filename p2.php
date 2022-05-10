31a3be661afaee3ee9242e61975089d05c13f0e6
Ivana182 <103907015+Ivana182@users.noreply.github.com>
2022-05-10 02:59:07 +0200

    Update p2.php

---
+++ p2.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+
+<title> Condicion IF </title>
+
+</head>
+
+<body>
+
+<h1> condicional IF </h1>
+
+<?PHP
+
+ $a=8;
+
+ $b=3;
+
+Echo "valor de a:",$a,"y el valor de b:",$b,"<br>";
+
+If ($a<$b)
+
+{
+
+echo "a es menor que b";
+
+}
+
+else
+
+{
+
+echo "a no es menor que b";
+
+}
+
+?>
+
+</body>
+
+</html>
