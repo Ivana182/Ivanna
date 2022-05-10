f54d83ad5cf9792da972d90a2fd961239fd3398e
Ivana182 <103907015+Ivana182@users.noreply.github.com>
2022-05-10 03:18:05 +0200

    Update p3.php

--- p3.php
+++ p3.php
@@ -1,1 +1,27 @@
-yh
+<html>
+
+<head>
+
+<title></title>
+
+</head>
+
+<body>
+
+<h1> Segundo ejemplo de variables usando el operador concatenación.</h1>
+
+<p>
+
+<?php
+
+$Name="Miguel";
+
+Echo "hola <b>". $Name. "</b>, encantado de conocerte";
+
+?>
+
+</p>
+
+</body>
+
+</html>
